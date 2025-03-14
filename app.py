39d37a4db158308fbe170b8640ab00e57a44093c
Fujijared2810 <scorpeejared2@gmail.com>
2025-03-15 00:51:57 +0800

    pls

--- app.py
+++ app.py
@@ -866,4 +866,4 @@ if __name__ == '__main__':
     
     # Run the socketio app
     port = int(os.environ.get('PORT', 8081))
-    socketio.run(app, host='0.0.0.0', port=port, debug=False, allow_unsafe_werkzeug=True)
+    socketio.run(app, host='0.0.0.0', port=port, debug=False)
